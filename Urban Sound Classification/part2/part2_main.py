57557ec8fd2b056f5ba174d6282aa2d2ab17644e
sglee487 <sglee487@gmail.com>
2019-05-10 15:46:25 +0900

    fix error. just custom variants...

--- Urban Sound Classification/part2/part2_main.py
+++ Urban Sound Classification/part2/part2_main.py
@@ -73,3 +73,111 @@ ts_labels = one_hot_encode(ts_labels)
 
 print ("middle")
 
+# ========================================
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev = 0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(1.0, shape = shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x,W,strides=[1,2,2,1], padding='SAME')
+
+def apply_convolution(x,kernel_size,num_channels,depth):
+    weights = weight_variable([kernel_size, kernel_size, num_channels, depth])
+    biases = bias_variable([depth])
+    return tf.nn.relu(tf.add(conv2d(x, weights),biases))
+
+def apply_max_pool(x,kernel_size,stride_size):
+    return tf.nn.max_pool(x, ksize=[1, kernel_size, kernel_size, 1],
+                          strides=[1, stride_size, stride_size, 1], padding='SAME')
+
+
+# ==============================================
+
+
+frames = 41
+bands = 60
+
+feature_size = 2460 #60x41
+# num_labels = 10
+num_labels = 2
+num_channels = 2
+
+batch_size = 50
+kernel_size = 30
+depth = 20
+num_hidden = 200
+
+learning_rate = 0.01
+total_iterations = 2000
+
+
+# ==============================================
+
+
+X = tf.placeholder(tf.float32, shape=[None,bands,frames,num_channels])
+Y = tf.placeholder(tf.float32, shape=[None,num_labels])
+# Y = tf.placeholder(tf.float32, shape=(18,2))
+# ValueError: Dimensions must be equal, but are 2 and 10 for 'mul' (op: 'Mul') with input shapes: [18,2], [?,10].
+# 이런 오류가 뜸.
+# 144 줄에서 loss에서 곱할때 오류가 뜨는걸 확인.
+# 오류 해결!!!!!!! 위의 변수 지정할 때 내가 custom 해서 변수를 지정하는 것이었다.
+# 그러므로 다른 코드들 다 원래대로 돌려놓고 하니 되긴 됨.
+# fold1, fold2, fold3 에 무슨 wav를 넣어야 되는지 알아야 된다.
+
+cov = apply_convolution(X,kernel_size,num_channels,depth)
+
+shape = cov.get_shape().as_list()
+cov_flat = tf.reshape(cov, [-1, shape[1] * shape[2] * shape[3]])
+
+f_weights = weight_variable([shape[1] * shape[2] * depth, num_hidden])
+f_biases = bias_variable([num_hidden])
+f = tf.nn.sigmoid(tf.add(tf.matmul(cov_flat, f_weights),f_biases))
+
+out_weights = weight_variable([num_hidden, num_labels])
+out_biases = bias_variable([num_labels])
+y_ = tf.nn.softmax(tf.matmul(f, out_weights) + out_biases)
+# y_ = Tensor("Softmax:0", shape=(?, 10), dtype=float32)
+# f 는 (?,200), out_weights은 (200,10) 차워인데 num_hidden = 200, num_labels = 10 이다. 이 두 변수를 조절해서 (18,2)로 강제로 맞춰보자.
+
+
+# ============================================
+
+
+loss = -tf.reduce_sum(Y * tf.log(y_))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+correct_prediction = tf.equal(tf.argmax(y_,1), tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+# =========================================
+
+
+cost_history = np.empty(shape=[1], dtype=float)
+with tf.Session() as session:
+    tf.initialize_all_variables().run()
+
+    for itr in range(total_iterations):
+        offset = (itr * batch_size) % (tr_labels.shape[0] - batch_size)
+        batch_x = tr_features[offset:(offset + batch_size), :, :, :]
+        batch_y = tr_labels[offset:(offset + batch_size), :]
+
+        _, c = session.run([optimizer, loss], feed_dict={X: batch_x, Y: batch_y})
+        # ValueError: Cannot feed value of shape(18, 2) for Tensor 'Placeholder_1:0', which has shape '(?, 10)' 에러가 뜬다.
+        # X = Tensor("Placeholder:0", shape=(?, 60, 41, 2), dtype=float32)
+        # Y = Tensor("Placeholder_1:0", shape=(?, 10), dtype=float32)
+        # 이므로 Y 텐서 차원을 (18,2) 로 바꾸면 되지 않을까? 한번 해 보자.
+        cost_history = np.append(cost_history, c)
+
+    print('Test accuracy: ', round(session.run(accuracy, feed_dict={X: ts_features, Y: ts_labels}), 3))
+    fig = plt.figure(figsize=(15, 10))
+    plt.plot(cost_history)
+    plt.axis([0, total_iterations, 0, np.max(cost_history)])
+    plt.show()
+
+
+# =====================================
