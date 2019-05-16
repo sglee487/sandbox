add09cd44a56aaad49f768afa8c6e811c7d6d845
sglee487 <sglee487@gmail.com>
2019-05-16 17:30:54 +0900

    learn more about melspectrogram

--- librosa_learn/librosa.feature.melspectrogram.py
+++ librosa_learn/librosa.feature.melspectrogram.py
@@ -9,7 +9,11 @@ import scipy.fftpack
 
 
 # y, sr = librosa.load(librosa.util.example_audio_file())
-y, sr = librosa.load('CantinaBand3.wav')
+C = 'CantinaBand3.wav'
+S = 'sample.wav'
+St = 'StarWars3.wav'
+y, sr = librosa.load(S)
+# https://librosa.github.io/librosa/tutorial.html
 
 librosa.feature.melspectrogram(y=y, sr=sr)
 # array([[  2.891e-07,   2.548e-03, ...,   8.116e-09,   5.633e-09],
@@ -20,8 +24,10 @@ librosa.feature.melspectrogram(y=y, sr=sr)
 
 # Using a pre-computed power spectrogram
 
-D = np.abs(librosa.stft(y))**2
-S = librosa.feature.melspectrogram(S=D)
+t = (librosa.stft(y)) # https://librosa.github.io/librosa/generated/librosa.core.stft.html?highlight=stft#librosa.core.stft
+
+D = np.abs(librosa.stft(y))**2 # 위에 fourier fransform 하면 허수가 생겨서 제곱하는듯.
+S = librosa.feature.melspectrogram(S=D) # https://librosa.github.io/librosa/generated/librosa.feature.melspectrogram.html
 
 # Passing through arguments to the Mel filters
 S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,
@@ -29,6 +35,7 @@ S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,
 
 import matplotlib.pyplot as plt
 plt.figure(figsize=(10, 4))
+# https://librosa.github.io/librosa/generated/librosa.core.power_to_db.html
 librosa.display.specshow(librosa.power_to_db(S,
                                              ref=np.max),
                          y_axis='mel', fmax=8000,
